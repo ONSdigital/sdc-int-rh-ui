6d4283384826b7ff2406f7b8c00ecc405d3eda87
Paul-Joel <paul.joel@ons.gov.uk>
2021-05-24 15:14:38 +0100

    SOCINT-4 Updates to remove AD Lookup

--- app/routes.py
+++ app/routes.py
@@ -6,14 +6,13 @@ from .request_handlers import request_routes
 from .start_handlers import start_routes
 from .handlers import static_routes
 from .common_handlers import common_routes
-from .support_centre_handlers import support_centre_routes
 
 
 def setup(app, url_path_prefix):
     """Set up routes as resources so we can use the `Index:get` notation for URL lookup."""
 
     combined_routes = [*request_routes, *start_routes, *static_routes, *web_form_routes,
-                       *webchat_routes, *common_routes, *support_centre_routes]
+                       *webchat_routes, *common_routes]
 
     for route in combined_routes:
         use_prefix = route.kwargs.get('use_prefix', True)
