ef6b6b78893b806f2783295a4a5d8b4da36ae35a
Peter Bochel <Peter.Bochel@ext.ons.gov.uk>
2021-08-31 15:23:59 +0100

    SOCINT-55 Getting Redis & RHUI running

--- docker/rh-ui-stop.sh
+++ docker/rh-ui-stop.sh
@@ -7,6 +7,7 @@ set -e
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-docker compose -f $SCRIPT_DIR/docker-compose-mock-ai.yml stop
-
+echo "1/1 Stopping services"
 docker compose -f $SCRIPT_DIR/docker-compose-rh-ui.yml stop
+docker compose -f $SCRIPT_DIR/docker-compose-mock-ai.yml stop
+docker compose -f $SCRIPT_DIR/docker-compose-redis.yml stop
--- docker/rh-ui-up.sh
+++ docker/rh-ui-up.sh
@@ -13,8 +13,25 @@ set -e
 #   - RH-service is running.
 #
 
+MOCK_AI_VERSION="europe-west2-docker.pkg.dev/ons-ci-int/int-docker-release/mock-ai:latest"
+RH_UI_VERSION="europe-west2-docker.pkg.dev/ons-ci-int/int-docker-release/rh-ui:latest"
+
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-docker compose -f $SCRIPT_DIR/docker-compose-mock-ai.yml up -d
 
+echo "1/4 Checking environment variables"
+[ -z "$DOCKER_GCP_CREDENTIALS" ] && echo "Error: DOCKER_GCP_CREDENTIALS must be set" && exit 1;
+[ -z "$GOOGLE_CLOUD_PROJECT" ] && echo "Need to set GOOGLE_CLOUD_PROJECT" && exit 1;
+
+echo "2/4 Pulling images ..."
+docker pull $MOCK_AI_VERSION
+docker pull $RH_UI_VERSION
+
+echo "3/4 Tagging images ..."
+docker tag $MOCK_AI_VERSION mock-ai
+docker tag $RH_UI_VERSION rh-ui
+
+echo "4/4 Starting services ..."
+docker compose -f $SCRIPT_DIR/docker-compose-redis.yml up -d
+docker compose -f $SCRIPT_DIR/docker-compose-mock-ai.yml up -d
 docker compose -f $SCRIPT_DIR/docker-compose-rh-ui.yml up -d
