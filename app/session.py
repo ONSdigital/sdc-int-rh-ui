773ad4440b71ae7c098a7b98f5bf6ed24f734de0
KieranWardle <23309814+KieranWardle@users.noreply.github.com>
2022-05-05 09:39:37 +0100

    Upgraded aioredis version

--- app/session.py
+++ app/session.py
@@ -1,7 +1,8 @@
 import time
 
 from asyncio import get_event_loop
-from aioredis import create_pool, RedisError
+
+from aioredis import from_url, RedisError
 from aiohttp_session import session_middleware, Session, get_session
 from aiohttp_session.redis_storage import RedisStorage
 from structlog import get_logger
@@ -16,49 +17,46 @@ logger = get_logger('respondent-home')
 # Monkey patch aiohttp_session Session.__init__ method to remove suspect behaviour.
 
 
-def aiohttp_session_pr_331_rollback(self, identity, *, data, new,
-                                    max_age=None):
-    self._changed = False
-    self._mapping = {}
-    self._identity = identity if data != {} else None
-    self._new = new
-    self._new = new if data != {} else True
-    self._max_age = max_age
-    created = data.get('created', None) if data else None
-    session_data = data.get('session', None) if data else None
-
-    if self._new or created is None:
-        self._created = int(time.time())
-    else:
-        self._created = created
-
-    if session_data is not None:
-        self._mapping.update(session_data)
+# def aiohttp_session_pr_331_rollback(self, identity, *, data, new,
+#                                     max_age=None):
+#     self._changed = False
+#     self._mapping = {}
+#     self._identity = identity if data != {} else None
+#     self._new = new
+#     self._new = new if data != {} else True
+#     self._max_age = max_age
+#     created = data.get('created', None) if data else None
+#     session_data = data.get('session', None) if data else None
+#
+#     if self._new or created is None:
+#         self._created = int(time.time())
+#     else:
+#         self._created = created
+#
+#     if session_data is not None:
+#         self._mapping.update(session_data)
 
 
 def setup(app_config):
     # Monkey patch aiohttp_session.py Session.__init__ method to remove PR 331 as above
-    Session.__init__ = aiohttp_session_pr_331_rollback
+    # Session.__init__ = aiohttp_session_pr_331_rollback
 
     loop = get_event_loop()
     redis_pool = loop.run_until_complete(
-        make_redis_pool(app_config['REDIS_SERVER'], app_config['REDIS_PORT'], app_config['REDIS_POOL_MIN'], app_config['REDIS_POOL_MAX']))
+        make_redis_pool(app_config['REDIS_SERVER'], app_config['REDIS_PORT']))
     return session_middleware(
         RedisStorage(redis_pool,
                      cookie_name='RH_SESSION',
                      max_age=int(app_config['SESSION_AGE'])))
 
 
-async def make_redis_pool(host, port, poolMin, poolMax):
-    redis_address = (host, port)
+async def make_redis_pool(host, port):
+    redis_host = "redis://" + host + ":" + port
     try:
-        redis_pool = await create_pool(
-            redis_address,
-            create_connection_timeout=3,
-            minsize=int(poolMin),
-            maxsize=int(poolMax)
+        redis = from_url(
+            redis_host, encoding="utf-8", decode_responses=True
         )
-        return redis_pool
+        return redis
     except (OSError, RedisError):
         logger.error('failed to create redis connection')
 
