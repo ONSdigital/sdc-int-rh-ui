7bc84233dca8b111430a6b6475d1f8ed866c2dba
KieranWardle <23309814+KieranWardle@users.noreply.github.com>
2022-05-18 12:06:42 +0100

    Removed commented out code

--- app/session.py
+++ app/session.py
@@ -17,30 +17,7 @@ logger = get_logger('respondent-home')
 # Monkey patch aiohttp_session Session.__init__ method to remove suspect behaviour.
 
 
-def aiohttp_session_pr_331_rollback(self, identity, *, data, new,
-                                    max_age=None):
-    self._changed = False
-    self._mapping = {}
-    self._identity = identity if data != {} else None
-    self._new = new
-    self._new = new if data != {} else True
-    self._max_age = max_age
-    created = data.get('created', None) if data else None
-    session_data = data.get('session', None) if data else None
-
-    if self._new or created is None:
-        self._created = int(time.time())
-    else:
-        self._created = created
-
-    if session_data is not None:
-        self._mapping.update(session_data)
-
-
 def setup(app_config):
-    # Monkey patch aiohttp_session.py Session.__init__ method to remove PR 331 as above
-    # Session.__init__ = aiohttp_session_pr_331_rollback
-
     loop = asyncio.get_event_loop()
     redis_pool = loop.run_until_complete(
         make_redis_pool(app_config['REDIS_SERVER'], app_config['REDIS_PORT']))
