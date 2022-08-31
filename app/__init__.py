e18eb01d8c3d9921ce34e34eb954d497ce9d5983
gavinleeedwards <gavin.edwards@ons.gov.uk>
2022-08-31 13:45:53 +0100

    Remove a link and add a contacts section to the main page (#58)

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 VERSION = '0.0.1'
 
-START_PAGE_TITLE_EN = 'Start survey'
-START_PAGE_TITLE_CY = 'Start survey'
+START_PAGE_TITLE_EN = 'Start study'
+START_PAGE_TITLE_CY = 'Start study'
 
 BAD_CODE_MSG = {'text': 'Enter an access code', 'clickable': True, 'level': 'ERROR', 'type': 'BAD_CODE', 'field': 'uac_empty'}  # NOQA
 INVALID_CODE_MSG = {'text': 'Enter a valid access code', 'clickable': True, 'level': 'ERROR', 'type': 'INVALID_CODE', 'field': 'uac_invalid'}  # NOQA
