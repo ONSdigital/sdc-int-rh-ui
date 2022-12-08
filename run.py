df2614038c6e744aab33d58bee8225799257b8ec
Kacper Prywata <kacper.prywata@ons.gov.uk>
2022-12-08 09:58:22 +0000

    Create app with dev config when running locally

--- run.py
+++ run.py
@@ -3,5 +3,5 @@ from aiohttp import web
 if __name__ == '__main__':
     from app.app import create_app
 
-    app = create_app()
+    app = create_app('DevelopmentConfig')
     web.run_app(app, port=app['PORT'])
