45a81fcff6c1787e94f61ed7864e411022cfffcf
Kacper Prywata <kacper.prywata@ons.gov.uk>
2022-12-09 13:22:09 +0000

    Remove passing dev config

--- run.py
+++ run.py
@@ -3,5 +3,5 @@ from aiohttp import web
 if __name__ == '__main__':
     from app.app import create_app
 
-    app = create_app('DevelopmentConfig')
+    app = create_app()
     web.run_app(app, port=app['PORT'])
