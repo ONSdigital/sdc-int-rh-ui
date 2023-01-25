0849c07543b062fc39ad716f16fe63c317a6baff
liamtoozer <liam.toozer@gmail.com>
2023-01-25 10:41:05 +0000

    Add new invalid UAC translation

--- app/constants.py
+++ app/constants.py
@@ -9,8 +9,8 @@ BAD_CODE_MSG_CY = {'text': 'PLACEHOLDER WELSH Enter an access code', 'clickable'
 NO_SELECTION_CHECK_MSG = {'text': 'Select an answer', 'level': 'ERROR', 'type': 'NO_SELECTION_ERROR', 'field': 'no-selection'}  # NOQA
 NO_SELECTION_CHECK_MSG_CY = {'text': "PLACEHOLDER WELSH Select an answer", 'level': 'ERROR', 'type': 'NO_SELECTION_ERROR', 'field': 'no-selection'}  # NOQA
 
-INVALID_CODE_MSG = {'text': 'Enter a valid access code', 'clickable': True, 'level': 'ERROR', 'type': 'INVALID_CODE', 'field': 'uac_invalid'}  # NOQA
-INVALID_CODE_MSG_CY = {'text': 'PLACEHOLDER WELSH Enter a valid access code', 'clickable': True, 'level': 'ERROR', 'type': 'INVALID_CODE', 'field': 'uac_invalid'}  # NOQA
+INVALID_CODE_MSG = {'text': 'Access code not recognised. Enter the code again.', 'clickable': True, 'level': 'ERROR', 'type': 'INVALID_CODE', 'field': 'uac_invalid'}  # NOQA
+INVALID_CODE_MSG_CY = {'text': 'Nid yw’r cod mynediad yn cael ei gydnabod. Rhowch y cod eto.', 'clickable': True, 'level': 'ERROR', 'type': 'INVALID_CODE', 'field': 'uac_invalid'}  # NOQA
 
 BAD_CODE_LENGTH_MSG = {'text': 'Enter a 16-character access code', 'clickable': True, 'level': 'ERROR', 'type': 'INVALID_CODE_LENGTH', 'field': 'uac_invalid_length'}  # NOQA
 BAD_CODE_LENGTH_MSG_CY = {'text': 'Rhowch god mynediad 16 nod', 'clickable': True, 'level': 'ERROR', 'type': 'INVALID_CODE_LENGTH', 'field': 'uac_invalid_length'}  # NOQA
