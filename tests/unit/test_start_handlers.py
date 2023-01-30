7d103f8afe8d19a49bf6aeee2c0e99199d4029a2
liamtoozer <liam.toozer@gmail.com>
2023-01-30 09:25:01 +0000

    671 Add invalid length and malformed UAC error message translation (#100)

--- tests/unit/test_start_handlers.py
+++ tests/unit/test_start_handlers.py
@@ -1,7 +1,7 @@
 from aiohttp.client_exceptions import ClientConnectionError
 from aioresponses import aioresponses
 
-from app.constants import BAD_CODE_MSG, INVALID_CODE_MSG
+from app.constants import BAD_CODE_MSG, INVALID_CODE_MSG, BAD_CODE_LENGTH_MSG, BAD_CODE_LENGTH_MSG_CY, BAD_CODE_MSG_CY
 from app.start_handlers import Start
 from .helpers import TestHelpers
 
@@ -19,7 +19,7 @@ class TestStartHandlers(TestHelpers):
                        '/54598f02da027026a584fd0bc7176de55a3e6472f4b3c74f68d0ae7be206e17c' \
                        '?accountServiceUrl=http://localhost:9092&languageCode=cy'
 
-    async def test_post_start_invalid_blank_ew(self):
+    async def test_post_start_invalid_blank_en(self):
         form_data = self.start_data_valid.copy()
         form_data['uac'] = ''
 
@@ -34,6 +34,54 @@ class TestStartHandlers(TestHelpers):
         self.assertSiteLogo('en', contents)
         self.assertCorrectTranslationLink(contents, 'en', self.user_journey)
         self.assertMessagePanel(BAD_CODE_MSG, contents)
+
+    async def test_post_start_invalid_blank_cy(self):
+        form_data = self.start_data_valid.copy()
+        form_data['uac'] = ''
+
+        with self.assertLogs('respondent-home', 'INFO') as cm:
+            response = await self.client.request('POST',
+                                                 self.post_start_cy,
+                                                 data=form_data)
+        self.assertLogEvent(cm, 'access code not supplied')
+
+        self.assertEqual(response.status, 200)
+        contents = str(await response.content.read())
+        self.assertSiteLogo('cy', contents)
+        self.assertCorrectTranslationLink(contents, 'cy', self.user_journey)
+        self.assertMessagePanel(BAD_CODE_MSG_CY, contents)
+
+    async def test_post_start_invalid_length_en(self):
+        form_data = self.start_data_valid.copy()
+        form_data['uac'] = '1'
+
+        with self.assertLogs('respondent-home', 'INFO') as cm:
+            response = await self.client.request('POST',
+                                                 self.post_start_en,
+                                                 data=form_data)
+        self.assertLogEvent(cm, 'attempt to use a malformed access code')
+
+        self.assertEqual(response.status, 200)
+        contents = str(await response.content.read())
+        self.assertSiteLogo('en', contents)
+        self.assertCorrectTranslationLink(contents, 'en', self.user_journey)
+        self.assertMessagePanel(BAD_CODE_LENGTH_MSG, contents)
+
+    async def test_post_start_invalid_length_cy(self):
+        form_data = self.start_data_valid.copy()
+        form_data['uac'] = '1'
+
+        with self.assertLogs('respondent-home', 'INFO') as cm:
+            response = await self.client.request('POST',
+                                                 self.post_start_cy,
+                                                 data=form_data)
+        self.assertLogEvent(cm, 'attempt to use a malformed access code')
+
+        self.assertEqual(response.status, 200)
+        contents = str(await response.content.read())
+        self.assertSiteLogo('cy', contents)
+        self.assertCorrectTranslationLink(contents, 'cy', self.user_journey)
+        self.assertMessagePanel(BAD_CODE_LENGTH_MSG_CY, contents)
 
     async def test_post_start_invalid_text_url_ew(self):
         form_data = self.start_data_valid.copy()
@@ -149,31 +197,31 @@ class TestStartHandlers(TestHelpers):
         self.check_content_start('cy', str(await response.content.read()), check_error=True)
 
     def test_uac_hash(self):
-        # Given some post data
-        post_data = {'uac': 'w4nw wpph jjpt p7fn', 'action[save_continue]': ''}
+        # Given
+        uac = 'W4NWWPPHJJPTP7FN'
 
         # When join_uac is called
-        result = Start._uac_hash(post_data['uac'])
+        result = Start._uac_hash(uac)
 
         # Then a single string built from the uac values is returned
         self.assertEqual(result, '54598f02da027026a584fd0bc7176de55a3e6472f4b3c74f68d0ae7be206e17c')
 
     def test_join_uac_missing(self):
         # Given some missing post data
-        post_data = {'uac': '', 'action[save_continue]': ''}
+        uac = ''
 
         # When join_uac is called
         with self.assertRaises(TypeError):
-            Start._uac_hash(post_data['uac'])
+            Start._uac_hash(uac)
         # Then a TypeError is raised
 
     def test_join_uac_some_missing(self):
-        # Given some missing post data
-        post_data = {'uac': '123456781234', 'action[save_continue]': ''}
+        # Given
+        uac = '123456781234'
 
         # When join_uac is called
         with self.assertRaises(TypeError):
-            Start._uac_hash(post_data['uac'])
+            Start._uac_hash(uac)
         # Then a TypeError is raised
 
     async def test_get_signed_out_en(self):
