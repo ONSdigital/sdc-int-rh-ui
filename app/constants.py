670b46b771b446c5ced712592324a53cc4e9575b
Kacper Prywata <kacper.prywata@ons.gov.uk>
2023-01-31 15:35:22 +0000

    Remove unused variables

--- app/constants.py
+++ app/constants.py
@@ -6,9 +6,6 @@ START_PAGE_TITLE_CY = 'PLACEHOLDER WELSH Start study'
 BAD_CODE_MSG = {'text': 'Enter an access code', 'clickable': True, 'level': 'ERROR', 'type': 'BAD_CODE', 'field': 'uac_empty'}  # NOQA
 BAD_CODE_MSG_CY = {'text': 'PLACEHOLDER WELSH Enter an access code', 'clickable': True, 'level': 'ERROR', 'type': 'BAD_CODE', 'field': 'uac_empty'}  # NOQA
 
-NO_SELECTION_CHECK_MSG = {'text': 'Select an answer', 'level': 'ERROR', 'type': 'NO_SELECTION_ERROR', 'field': 'no-selection'}  # NOQA
-NO_SELECTION_CHECK_MSG_CY = {'text': "PLACEHOLDER WELSH Select an answer", 'level': 'ERROR', 'type': 'NO_SELECTION_ERROR', 'field': 'no-selection'}  # NOQA
-
 INVALID_CODE_MSG = {'text': 'Access code not recognised. Enter the code again.', 'clickable': True, 'level': 'ERROR', 'type': 'INVALID_CODE', 'field': 'uac_invalid'}  # NOQA
 INVALID_CODE_MSG_CY = {'text': "Nid yw'r cod mynediad yn cael ei gydnabod. Rhowch y cod eto.", 'clickable': True, 'level': 'ERROR', 'type': 'INVALID_CODE', 'field': 'uac_invalid'}  # NOQA
 
