96c1b5f54518702c5327fb703c1e4f3b8e5dbd73
Kacper Prywata <kacper.prywata@ons.gov.uk>
2023-02-17 08:36:47 +0000

    Linting

--- app/start_handlers.py
+++ app/start_handlers.py
@@ -8,7 +8,7 @@ from app.constants import (BAD_CODE_MSG, BAD_CODE_MSG_CY, INVALID_CODE_MSG, INVA
                            START_PAGE_TITLE_EN, UAC_LENGTH, BAD_CODE_LENGTH_MSG, BAD_CODE_LENGTH_MSG_CY)
 from app.eq import EqLaunch
 from app.flash import flash
-from app.security import get_sha256_hash, invalidate
+from app.security import get_sha256_hash
 from app.utils import View
 
 logger = get_logger('respondent-home')
@@ -97,4 +97,3 @@ class Start(View):
             flash(request, BAD_CODE_MSG)
 
         return HTTPFound(request.app.router['Start:get'].url_for(display_region=display_region))
-
