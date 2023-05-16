1f87e5215bdb68db9b8e8991f578122beec74dda
Kacper Prywata <kacper.prywata@ons.gov.uk>
2023-05-16 16:03:24 +0100

    Change the format of the gmt-tag-id variable in the tests

--- tests/unit/test_google_analytics.py
+++ tests/unit/test_google_analytics.py
@@ -7,29 +7,29 @@ from tests.unit import RHTestCase
 class TestGoogleAnalytics(RHTestCase):
     async def test_google_analytics_context(self):
         self.app['GTM_CONTAINER_ID'] = 'GTM-XXXXXXX'
-        self.app['GTM_TAG_ID'] = '12345'
+        self.app['GTM_TAG_ID'] = 'G-XXXXXXXXXX'
         request = make_mocked_request('GET', '/', app=self.app)
         context = await ga_ua_id_processor(request)
         self.assertEqual(context['gtm_cont_id'], 'GTM-XXXXXXX')
-        self.assertEqual(context['gtm_tag_id'], '12345')
+        self.assertEqual(context['gtm_tag_id'], 'G-XXXXXXXXXX')
 
     async def test_google_analytics_script_rendered_base_en(self):
         self.app['GTM_CONTAINER_ID'] = 'GTM-XXXXXXX'
-        self.app['GTM_TAG_ID'] = 'G-1234567890'
+        self.app['GTM_TAG_ID'] = 'G-XXXXXXXXXX'
         response = await self.client.request('GET', self.get_start_en)
         self.assertEqual(response.status, 200)
         response = await response.content.read()
         self.assertIn("(window,document,\'script\',\'dataLayer\',\'GTM-XXXXXXX\');".encode(), response)
-        self.assertIn("https://www.googletagmanager.com/gtag/js?id=G-1234567890".encode(), response)
+        self.assertIn("https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX".encode(), response)
 
     async def test_google_analytics_script_rendered_base_cy(self):
         self.app['GTM_CONTAINER_ID'] = 'GTM-XXXXXXX'
-        self.app['GTM_TAG_ID'] = '12345'
+        self.app['GTM_TAG_ID'] = 'G-XXXXXXXXXX'
         response = await self.client.request('GET', self.get_start_cy)
         self.assertEqual(response.status, 200)
         response = await response.content.read()
         self.assertIn("(window,document,\'script\',\'dataLayer\',\'GTM-XXXXXXX\');".encode(), response)
-        self.assertIn("https://www.googletagmanager.com/gtag/js?id=12345".encode(), response)
+        self.assertIn("https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX".encode(), response)
 
     async def test_google_analytics_script_not_rendered_missing_container_id_base_en(self):
         self.app['GTM_CONTAINER_ID'] = ''
