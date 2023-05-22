b223734673ed46315bba25b2b5eca09eeeee7543
Kacper Prywata <kacper.prywata@ons.gov.uk>
2023-05-22 12:25:29 +0100

    Clean up CSP headers

--- app/security.py
+++ app/security.py
@@ -17,33 +17,25 @@ CSP = {
     'font-src': [
         "'self'",
         'data:',
-        'https://fonts.gstatic.com',
         'https://cdn.ons.gov.uk',
     ],
     'script-src': [
         "'self'",
-        'https://www.googletagmanager.com',
         'https://*.googletagmanager.com',
         'https://cdn.ons.gov.uk',
     ],
     'connect-src': [
         "'self'",
         'https://cdn.ons.gov.uk',
-        'https://*.google-analytics.com', 
-        'https://*.analytics.google.com', 
+        'https://*.google-analytics.com',
+        'https://*.analytics.google.com',
         'https://*.googletagmanager.com'
-    ],
-    'frame-src': [
-        'https://www.googletagmanager.com',
-        'https://www.timeforstorm.com'
     ],
     'img-src': [
         "'self'",
         'data:',
         'https://*.google-analytics.com',
         'https://*.googletagmanager.com',
-        'https://ssl.gstatic.com',
-        'https://www.gstatic.com',
         'https://cdn.ons.gov.uk'
     ],
 
